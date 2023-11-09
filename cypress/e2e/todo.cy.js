5c8c57efc1bbd2aca43c980ce2a7ecf4030d6f3b
simon <simon@mindmore.com>
2023-11-09 13:38:43 +0100

    init

---
+++ cypress/e2e/todo.cy.js
@@ -0,0 +1,18 @@
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:8081/')
+  })
+
+  it('checks the resolution and window size', () => {
+    cy.contains('resolution is below')
+    cy.contains('window is below')
+  })
+
+
+  it('sets view port and checks the resolution and window size', () => {
+    cy.viewport(1900, 1050)
+    cy.contains('resolution is below')
+    cy.contains('window is above')
+  })
+})
